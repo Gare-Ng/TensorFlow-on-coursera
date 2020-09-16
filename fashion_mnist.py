d4863c5f106474afe85d6c136567588bf863fef5
Gare-Ng <1073177998@qq.com>
2020-09-16 13:17:47 +0800

    nothing

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -1,4 +1,17 @@
 import tensorflow as tf
-import tensorflow.python.keras as keras
+from tensorflow import keras
 fashion_mnist=keras.datasets.fashion_mnist
-(train_images,train_labels),(test_images,test_labels)=fashion_mnist.load_data()
+(training_images,training_labels),(test_images,test_labels)=fashion_mnist.load_data()
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128,activation=tf.nn.relu),
+    keras.layers.Dense(10,activation=tf.nn.softmax)
+])
+model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
